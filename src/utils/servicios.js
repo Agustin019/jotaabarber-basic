1e068e7d63e9c46d261e1ad174b3fc57966dc31d
agustinnarciande <narciande.2019@gmail.com>
2023-03-15 16:12:13 -0300

    Importando imagenes desde un host

--- src/utils/servicios.js
+++ src/utils/servicios.js
@@ -2,28 +2,28 @@ import image from '../assests/corte.jpg'
 export const SERVICIOS = [
     {
         id:1,
-        img:'../../src/../../src/assests/corte.jpg',
+        img:'https://i.ibb.co/PxBzMp4/corte.png',
         name:'Corte',
         description:'Con tijera, rasuradora o navaja',
         price:'1000'
     },
     {
         id:2,
-        img:'../../src/assests/corte-barba.jpg',
+        img:'https://i.ibb.co/fYB2jhG/corte-barba.png',
         name:'Corte y barba',
         description:'Con tijera, rasuradora o navaja',
         price:'1300'
     },
     {
         id:3,
-        img:'../../src/assests/corte.jpg',
+        img:'https://i.ibb.co/PxBzMp4/corte.png',
         name:'Claritos / Reflejos',
         description:'incluye corte de pelo',
         price:'4500'
     },
     {
         id:4,
-        img:'../../src/assests/corte.jpg',
+        img:'https://i.ibb.co/PxBzMp4/corte.png',
         name:'Color Global',
         description:'incluye corte de pelo',
         price:'6000'
