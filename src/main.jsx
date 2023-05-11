6946ffdbfe977ae5292d3b44f9cfe4443a31b3bc
agustinnarciande <narciande.2019@gmail.com>
2023-05-11 00:01:03 -0300

    Agregando la ruta privada

--- src/main.jsx
+++ src/main.jsx
@@ -1,74 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './components/layout'
-import Nosotros from './pages/nosotros'
-import Turnos from './pages/turnos'
-import Agenda from './pages/agenda'
-import AdministrarTurnos from './pages/administrarTurnos'
-import Inicio from './pages/inicio'
-import InicioAdmin from './pages/inicioAdmin'
-
-import { AuthProvider } from './context/authContext'
-import MiCuenta from './pages/miCuenta'
-import Usuario from './pages/client/usuario'
+import App from './app'
 
 
 
-const router = createBrowserRouter([
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App/> 
+);
 
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Inicio />
-      },
-      {
-        path: '/nosotros',
-        element: <Nosotros />
-      },
-      {
-        path: '/turnos',
-        element: <Turnos />
-      },
-      {
-        path:'/micuenta',
-        element:<MiCuenta/>
-      },
-      {
-        path:'/usuario',
-        element:<Usuario/>
-      }
-    ]
-  },
-  {
-    path: '/admin',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <InicioAdmin />
-      },
-      {
-        path: '/admin/agenda',
-        element: <Agenda />
-      },
-      {
-        path: '/admin/administrarturnos',
-        element: <AdministrarTurnos />
-      },
-    ]
-  }
-
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>,
-)
