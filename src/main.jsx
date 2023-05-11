1c5d2bdec0529b0a743075283729dd5f4cd529ed
agustinnarciande <narciande.2019@gmail.com>
2023-05-11 10:01:23 -0300

    Cambio la sintaxis de las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
+import { AuthProvider } from './context/authContext'
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App/> 
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
 
