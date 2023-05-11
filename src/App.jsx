d54cad8b991d85ff4379a845091680f3c874942a
agustinnarciande <narciande.2019@gmail.com>
2023-05-11 11:44:13 -0300

    Ruta Turnos privatizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider, BrowserRouter, Route, Routes } from 'react-router-dom'
+import {  BrowserRouter, Route, Routes } from 'react-router-dom'
+import PrivateRoute from './components/privateRoute'
 import Layout from './components/layout'
-import Nosotros from './pages/nosotros'
 import Turnos from './pages/turnos'
 import Agenda from './pages/agenda'
 import AdministrarTurnos from './pages/administrarTurnos'
 import Inicio from './pages/inicio'
 import InicioAdmin from './pages/inicioAdmin'
-import withAuth from './utils/auth/withAuth'; // importa el componente withAuth
-import { AuthProvider } from './context/authContext'
+
 import MiCuenta from './pages/miCuenta'
 import Usuario from './pages/client/usuario'
+import { useAuth } from './context/authContext'
 
 function App(){
 
-  /*  const router = createBrowserRouter([
-        
-        {
-            path: '/',
-            element: <Layout />,
-            children: [
-                {
-                    index: true,
-                    element: <Inicio />
-                },
-                {
-                    path: '/nosotros',
-                    element: <Nosotros />
-                },
-                {
-                    path: '/turnos',
-                    element: withAuth(Turnos) // envuelve la ruta de Turnos en el componente withAuth
-                },
-                {
-                    path: '/micuenta',
-                    element: <MiCuenta />
-                },
-                {
-                    path: '/usuario',
-                    element: <Usuario />
-                }
-            ]
-        },
-        {
-            path: '/admin',
-            element: <Layout />,
-            children: [
-                {
-                    index: true,
-                    element: <InicioAdmin />
-                },
-                {
-                    path: '/admin/agenda',
-                    element: withAuth(Agenda) // envuelve la ruta de Agenda en el componente withAuth
-                },
-                {
-                    path: '/admin/administrarturnos',
-                    element: withAuth(AdministrarTurnos) // envuelve la ruta de AdministrarTurnos en el componente withAuth
-                },
-            ]
-        }
-
-    ])*/
+const { user } = useAuth()
+console.log(user)
 
     return (
         <BrowserRouter>
@@ -71,7 +24,7 @@ function App(){
             <Routes>
                 //Cliente
                 <Route path='/' element={<Inicio/>}/>
-                <Route path='/turnos' element={<Turnos/>}/>
+                <Route path='/turnos' element={ user ?<Turnos/> :<MiCuenta/>}/> 
                 <Route path='/micuenta' element={<MiCuenta/>}/>
                 <Route path='/usuario' element={<Usuario/>}/>
 
