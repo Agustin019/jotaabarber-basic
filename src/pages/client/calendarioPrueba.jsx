c8b66267f9c9a77f6030ec70ef44a14b55676a35
agustinnarciande <narciande.2019@gmail.com>
2023-05-18 17:03:10 -0300

    Consulta de horarios disponibles por dia del calendario sleccionada + error del dia actual sin estar en el medio solucionado

--- src/pages/client/calendarioPrueba.jsx
+++ src/pages/client/calendarioPrueba.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import { generarDocumentoPorCadaDiaDisponible } from '../../utils/horariosLaborales'
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../utils/firebaseconfig';
 
 export default function SteoFechaYHora() {
+
+  const [turnos, setTurnos] = useState([])
+
+
   const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
   const monthsOfYear = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
@@ -10,12 +16,9 @@ export default function SteoFechaYHora() {
   if (!(currentDate instanceof Date && !isNaN(currentDate))) {
     currentDate = new Date();
   }
-  currentDate.setHours(0, 0, 0, 0);  
+  currentDate.setHours(0, 0, 0, 0);
   const [selectedDay, setSelectedDay] = useState(currentDate);
   const [selectedMonth, setSelectedMonth] = useState(monthsOfYear[selectedDay.getMonth()]);
- 
-
-
 
   const currentMonth = currentDate.getMonth();
   const currentDay = currentDate.getDay();
@@ -27,8 +30,10 @@ export default function SteoFechaYHora() {
     const middleDate = new Date(currentWeekStartDay.getFullYear(), currentWeekStartDay.getMonth(), currentWeekStartDay.getDate() + 3);
     setSelectedMonth(monthsOfYear[middleDate.getMonth()]);
   }, [currentWeekStartDay]);
-  
-  const fechaFormateada = format(selectedDay, 'dd/MM');
+  useEffect(() => {
+    handleDayClick(currentDate)
+  }, [])
+  const fechaFormateada = format(selectedDay, 'dd-MM');
   console.log(fechaFormateada)
 
   const handleDayClick = (day) => {
@@ -54,8 +59,8 @@ export default function SteoFechaYHora() {
       }
     });
   };
-    
-  
+
+
   const handleNextWeek = () => {
     setCurrentWeekStartDay((prevWeekStartDay) => {
       const newWeekStart = new Date(prevWeekStartDay.getFullYear(), prevWeekStartDay.getMonth(), prevWeekStartDay.getDate() + 4);
@@ -75,7 +80,7 @@ export default function SteoFechaYHora() {
     const startDate = new Date(currentWeekStartDay);
     const middleDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 3);
     const thirtyDaysLater = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 30);
-  
+
     for (let i = 0; i < 7; i++) {
       const date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + i);
       const day = date.getDate();
@@ -83,13 +88,11 @@ export default function SteoFechaYHora() {
       const isCurrentDay = date.toDateString() === currentDate.toDateString();
       const isMiddleDay = date.toDateString() === middleDate.toDateString();
       const isSelectable = date >= currentDate && date <= thirtyDaysLater;
-  
-      const dayClassNames = `py-2 px-4 mx-2 flex flex-col items-center gap-3 rounded-lg w-[63px] h-[76px] ${
-        isCurrentDay ? 'border border-[#1e1e1e]' : ''
-      } ${
-        isCurrentDay && isMiddleDay ? 'bg-[#1e1e1e] text-white' : isMiddleDay ? 'bg-[#1e1e1e] text-white' : ''
-      } ${!isSelectable ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`;
-  
+
+      const dayClassNames = `py-2 px-4 mx-2 flex flex-col items-center gap-3 rounded-lg w-[63px] h-[76px] ${isCurrentDay ? 'border border-[#1e1e1e]' : ''
+        } ${isCurrentDay && isMiddleDay ? 'bg-[#1e1e1e] text-white' : isMiddleDay ? 'bg-[#1e1e1e] text-white' : ''
+        } ${!isSelectable ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`;
+
       calendar.push(
         <div
           key={i}
@@ -101,19 +104,25 @@ export default function SteoFechaYHora() {
         </div>
       );
     }
-  
+
     return calendar;
   };
-  
+
   generarDocumentoPorCadaDiaDisponible()
-  // const availableDates = [];
-  // for (let date = currentDate; date <= thirtyDaysLater; date.setDate(date.getDate() + 1)) {
-  //   if (date.getDay() !== 0) { // Verificar si el día no es domingo (0: domingo, 1: lunes, ..., 6: sábado)
-  //     const formattedDate = format(date, 'dd-MM');
-  //     availableDates.push(formattedDate);
-  //   }
-  // }
-  // console.log(availableDates)
+
+
+
+
+  /*  consultar ´por los turnos disponibles segun el dia de hoy o la fecha seleccionada  */
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, 'horarios', fechaFormateada), (doc) => {
+      setTurnos(doc.data()?.horariosLaborales ?? []);
+      console.log(turnos)
+    });
+
+    return () => unsubscribe()
+  }, [selectedDay])
 
   return (
     <div className='w-full mx-auto flex flex-col items-center gap-y-8'>
@@ -122,6 +131,17 @@ export default function SteoFechaYHora() {
         <button className="text-xl" onClick={handlePrevWeek}><ion-icon name="arrow-back"></ion-icon></button>
         <div className="flex">{renderCalendar()}</div>
         <button className="text-xl" onClick={handleNextWeek}><ion-icon name="arrow-forward"></ion-icon></button>
+      </div>
+
+      <h2 className='font-semibold text-xl text-stone-800'>Turnos del dia {fechaFormateada}</h2>
+      <div className='w-full grid grid-cols-4 place-items-center'>
+        {
+          turnos?.map((turno, i) =>
+            <div key={i} className='w-24 h-12 bg-stone-700 rounded-lg'>
+              <p className='text-xl font-semibold text-white'>{turno.hora}</p>
+            </div>
+          )
+        }
       </div>
     </div>
   );
